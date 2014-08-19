5a2d0a2f261bca325b56110b831123f64ed3f43b
arty001 <arty_rajah@outlook.com>
2014-08-19 12:03:06 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -4,14 +4,3 @@ def whitesquare(side):
   for i in range(0,4):
     move(side)
     turn(90)
-
-def blacksquare(size):
-  
-  if size > 300:
-    return
-  
-  move(size)
-  turn(90)
-  blacksquare(size + 0.5)
-  
-blacksquare (0)
