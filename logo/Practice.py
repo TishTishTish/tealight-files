cf92a23d76b1401e34f9ba07d1c681ed6fb4e043
arty001 <arty_rajah@outlook.com>
2014-08-19 12:19:42 +0100

    Running logo/Practice.py

--- logo/Practice.py
+++ logo/Practice.py
@@ -1,1 +1,13 @@
-print "A new file!"
+from tealight.logo import move, turn
+
+def blacksquare(size):
+  
+  if size > 100:
+    return
+  
+  move(size)
+  turn(90)
+  blacksquare(size + 0.5)
+  
+blacksquare (0)
+
