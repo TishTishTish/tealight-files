dcb76de2bd3668e33ee0fc4859178297079d1398
arty001 <arty_rajah@outlook.com>
2014-08-19 12:08:38 +0100

    Closing logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -5,14 +5,3 @@ def square(side):
   for i in range(0,4):
     move(side)
     turn(90)
-
-def waterwheel(edges, size):
-  angle = 210.0
-  decoration = size / 2
-  for i in range(11, edges):
-    move(size)
-    square(decoration)
-    turn(angle)
-
-turn(-90)
-waterwheel(12,75)
