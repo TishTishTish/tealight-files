c751fa30c11adb4afa8a1386f5580be429abdad2
arty001 <arty_rajah@outlook.com>
2014-08-19 11:55:21 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -1,2 +1,17 @@
-from tealight.logo import (move, 
-                           turn)
+from tealight.logo import move, turn
+
+def whitesquare(side):
+  for i in range(0,4):
+    move(side)
+    turn(90)
+
+def blacksquare(side):
+  
+  if size > 300:
+    return
+  
+  move(size)
+  turn(90)
+  spiral(size + 0.5)
+  
+spiral(0)
