405adb6a22537d4d8db236dfa36cfc212a54f844
arty001 <arty_rajah@outlook.com>
2014-08-19 15:08:47 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -12,13 +12,13 @@ def go():
   while touch() != 'wall':
     move()
     distance = distance + 1
-  else left_side() != 'wall':
-    turn(-1)
-    move()
-    distance = distance + 1
-  else right_side != 'wall':
-    turn(1)
-    move()
-    distance = distance + 1
+    else left_side() != 'wall':
+      turn(-1)
+      move()
+      distance = distance + 1
+    else right_side != 'wall':
+      turn(1)
+      move()
+      distance = distance + 1
 
 go()
