fc270056b77b593b54b55caf627a4877bb94c913
arty001 <arty_rajah@outlook.com>
2014-08-20 12:23:56 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -1,4 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 color('green')
-box (250, 300, 100, 100)
+box(250, 300, 100, 100)
+
+color('black')
+line(250, 300, 100, 100)
