fa05dc892027f9917d3f666f57d096fd0013e0e6
arty001 <arty_rajah@outlook.com>
2014-08-20 12:30:01 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -1,8 +1,8 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 def greensquare():
-color('black')
-box(247, 298, 105, 105)
+  color('black')
+  box(247, 298, 105, 105)
 
-color('green')
-box(250, 300, 100, 100)
+  color('green')
+  box(250, 300, 100, 100)
