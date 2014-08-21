8b509a2d0e271fde0ccef3a0afea0db985e0dc94
arty001 <arty_rajah@outlook.com>
2014-08-21 15:08:57 +0100

    Closing art/Alalala.py

--- art/Alalala.py
+++
@@ -1,9 +0,0 @@
-#Networking
-from tealight.net import (connect, send)
-
-connect('Dinosaurs.py')
-message = 'Louise!!!'
-send(message)
-
-def handle_message(message):
-  print "Received message: " + str(message)
