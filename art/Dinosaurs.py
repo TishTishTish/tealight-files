71c9a2fa161a02d7df1faeafc4a4f32201c1c75c
arty001 <arty_rajah@outlook.com>
2014-08-21 14:15:50 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -1,10 +1,38 @@
-from tealight.art import (color, line, spot, circle, box, image, text, background)
+import sys
+from random import choice as randchoice
+from time import sleep
 
-def greensquare():
-  color('black')
-  box(247, 298, 105, 105)
+from utils import TextInput
 
-  color('green')
-  box(250, 300, 100, 100)
-  
-(greensquare())*8
+players    = 'XY'
+ai_players = 'Y'
+moves      = "rps"
+wins       = ("rp", "sr", "ps")   # choice on the right side wins
+status     = "%5s %3d %5s %3d     moves: %s %s"
+pause_time = 0.3
+
+
+class RockPaperScissors(object):
+     def run(self):
+         self.textinput = TextInput("(r|p|s)")
+         scores         = [0, 0]
+
+         while True:
+             choice1, choice2 = (self.get_move(p) for p in players)
+
+             if choice1 != choice2:
+                 winplayer = 1 if (choice1+choice2 in wins) else 0
+                 scores[winplayer] += 1
+
+             print(status % (players[0], scores[0], players[1], 
+scores[1], choice1, choice2))
+             sleep(pause_time)
+
+     def get_move(self, player):
+         if player in ai_players : return randchoice(moves)
+         else                    : return self.textinput.getval()
+
+
+if __name__ == "__main__":
+     try                      : RockPaperScissors().run()
+     except KeyboardInterrupt : pass
