187660c073924bbc23d4530c165d265f8702635b
arty001 <arty_rajah@outlook.com>
2014-08-21 14:46:12 +0100

    Running art/Alalala.py

--- art/Alalala.py
+++ art/Alalala.py
@@ -1,2 +1,8 @@
-print "A new file!"
-input()
+#Networking
+from tealight.net import (connect, send)
+
+connect(Dinosaurs.py)
+send(message, echo=False)
+
+def handle_message(message):
+  print "Received message: " + message
