1824ae428ba48b91e3163232e13de0b6d011d6e8
arty001 <arty_rajah@outlook.com>
2014-08-21 14:33:54 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -3,11 +3,8 @@ import random
 
 guessesTaken = 0
 
-print('Hello! What is your name?')
-myName = input()
 
 number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
 while guessesTaken < 6:
     print('Take a guess.') # There are four spaces in front of print.
