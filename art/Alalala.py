ed15ff594443f186c57b6a7e86dd784019100e8e
arty001 <arty_rajah@outlook.com>
2014-08-21 14:56:08 +0100

    Running art/Alalala.py

--- art/Alalala.py
+++ art/Alalala.py
@@ -3,7 +3,7 @@ from tealight.net import (connect, send)
 
 connect('Dinosaurs.py')
 message = int()
-send(message, str=False)
+send(message, echo=False)
 
 def handle_message(message):
   print "Received message: " + message
