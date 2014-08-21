9b5085cd4b32a94b10c5dde05f386082c2d47ffb
arty001 <arty_rajah@outlook.com>
2014-08-21 14:38:14 +0100

    Closing art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -1,31 +1,4 @@
+#Networking
+from tealight.net import connect, send
 
-import random
-
-guessesTaken = 0
-
-
-number = random.randint(1, 20)
-
-while guessesTaken < 6:
-    print('Take a guess.') # There are four spaces in front of print.
-    guess = input()
-    guess = int(guess)
-
-    guessesTaken = guessesTaken + 1
-
-    if guess < number:
-        print('Your guess is too low.') # There are eight spaces in front of print.
-
-    if guess > number:
-        print('Your guess is too high.')
-
-    if guess == number:
-        break
-
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
-
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+connect(
