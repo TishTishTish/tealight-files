e4fd4b66eaf542355a6ca53b4aef20c789d212ab
arty001 <arty_rajah@outlook.com>
2014-08-22 09:20:17 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -2,9 +2,12 @@
 from tealight.net import (connect, send)
 
 connect('Dinosaurs.py')
-message = "Louise!!!"
+message = "Do you want to play DinOTHELLOsaurus?"
 send(message)
 
 def handle_message(message):
   print "You have one message: " + str(message)
   
+def received_message:
+  if handle_message == "Yes"
+print "Let's play DinoOTHELLOsaurus!"
