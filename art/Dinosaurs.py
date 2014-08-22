ade2a95ed2ea02e29d7fec08976c893fdbebde48
arty001 <arty_rajah@outlook.com>
2014-08-22 09:39:54 +0100

    Closing art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -2,12 +2,10 @@
 from tealight.net import (connect, send)
 
 connect('Dinosaurs.py')
-message = "Do you want to play DinOTHELLOsaurus?"
 send(message)
-
-def handle_message(message):
-  print "You have one message: " + str(message)
-  
 def received_message(message):
   if message == "Yes" or "yes" or "Yes!":
   print "Let's play DinOTHELLOsaurus!"
+
+def handle_message(message):
+  print "You have one message: " + str(message)
