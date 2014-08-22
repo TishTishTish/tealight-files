cdcaebd97f8388b3e13c39c6f59b1c6b1f9e539c
arty001 <arty_rajah@outlook.com>
2014-08-22 09:21:15 +0100

    Running art/Dinosaurs.py

--- art/Dinosaurs.py
+++ art/Dinosaurs.py
@@ -9,5 +9,5 @@ def handle_message(message):
   print "You have one message: " + str(message)
   
 def received_message(message):
-  if handle_message = "Yes"
-print "Let's play DinoOTHELLOsaurus!"
+  if handle_message =! str("No")
+  print "Let's play DinoOTHELLOsaurus!"
